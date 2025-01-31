28b75d885fbd5d01e37e2671283a25db46a89860
taha <tahaqureshi0033@gmail.com>
2025-01-31 15:36:24 -0800

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,7 @@ import type React from "react" // Added import for React
 const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
-  title: "Beneficiary Management System",
+  title: "BMS",
   description: "A system for managing beneficiaries and their assistance requests",
 }
 
